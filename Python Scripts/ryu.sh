2af009e894fe66d6790f4498fddf5b9803c3b656
Alistair Drew <alistairdrew37@gmail.com>
2021-05-18 06:14:48 +0100

    update

---
+++ Python Scripts/ryu.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt install python-pip follow from ubunutu  
+
+sudo apt-get install python-ryu follow from ubunutu 
+
+pip install ryu 
+
+ryu-manager ryu.app.simple_switch_13 
+
